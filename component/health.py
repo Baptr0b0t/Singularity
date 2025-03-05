89ad63fe09699568fbd7694d670ff58ae3ad7c3b
Baptr0b0t <bapt.buriot@gmail.com>
2025-03-05 15:38:02 +0100

    Add max_health_point param to Health

--- component/health.py
+++ component/health.py
@@ -7,12 +7,13 @@ class Health(Gameobject.Component):
     """
     Contient les points de vie du joueur
     """
-    def __init__(self, parent, health_point=100):
+    def __init__(self, parent, health_point=100, max_health_point=100):
         super().__init__(parent)
         self.health_point = health_point
+        self.max_health_point = max_health_point
 
     def update(self):
-        self.health_point = min(100, self.health_point)
+        self.health_point = min(self.max_health_point, self.health_point)
 
 class HealthRegen(Gameobject.Component):
     def __init__(self,parent, regen_rate=1):
